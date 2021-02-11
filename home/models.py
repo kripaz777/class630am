cbea6bff25edffe4fe398c5401be836c8c112165
kripaz777 <aceraayush@gmail.com>
2021-02-11 08:53:04 +0545

    first commit

---
+++ home/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+LABEL = (('new','new'),('hot','hot'),('','default'))
+STATUS = (('active','active'),('','default'))
+STOCK = (('in','IN Stock'),('out','Out of stock'))
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 300)
+    slug = models.CharField(max_length = 300)
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    title = models.CharField(max_length = 300)
+    price = models.IntegerField()
+    discounted_price = models.IntegerField()
+    image = models.TextField(blank = True)
+    description = models.TextField(blank = True)
+    label = models.CharField(choices = LABEL,max_length = 100,blank = True)
+    slug = models.CharField(max_length= 200)
+    status = models.CharField(max_length = 300,choices = STATUS,blank = True)
+    stock = models.CharField(max_length = 300,choices = STOCK)
+
+    def __str__(self):
+        return self.title
+
+class Slider(models.Model):
+    name = models.CharField(max_length = 500)
+    image = models.TextField()
+    text = models.TextField()
+    rank = models.IntegerField()
+    status = models.CharField(max_length = 200,choices = STATUS,blank = True)
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length = 400)
+    image = models.TextField()
+    rank = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length = 300)
+    subject =models.TextField()
+    email = models.CharField(max_length = 200,blank = True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Ad(models.Model):
+    name = models.CharField(max_length = 400)
+    image = models.TextField()
+    rank = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
---
+++ home/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic import View
+# Create your views here.
+from .models import *
+
+class Baseview(View):
+    view = {}
+
+
+class HomeView(Baseview):
+    def get(self,request):
+        self.view['categories'] =  Category.objects.all()
+        self.view['sliders'] = Slider.objects.all()
+        self.view['items'] = Item.objects.all()
+        self.view['new'] = Item.objects.filter(label = 'new')
+        self.view['hot'] = Item.objects.filter(label='hot')
+        self.view['brands'] = Brand.objects.all()
+        self.view['rank_one'] = Ad.objects.filter(rank = 1)
+        self.view['rank_two'] = Ad.objects.filter(rank=2)
+        self.view['rank_three'] = Ad.objects.filter(rank=3)
+        self.view['rank_four'] = Ad.objects.filter(rank=4)
+        self.view['rank_five'] = Ad.objects.filter(rank=5)
+        self.view['rank_six'] = Ad.objects.filter(rank=6)
+        self.view['rank_seven'] = Ad.objects.filter(rank=7)
+        self.view['rank_eight'] = Ad.objects.filter(rank=8)
+        return render(request,'index.html',self.view)
